3ff7f97e44ea9a204b5c564fb5e587ee42fa8e39
Jerx2y <jerx2y@outlook.com>
2022-07-13 11:46:56 +0800

    2-image8 generated

--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -47,10 +47,56 @@ impl Perlin {
         p
     }
 
+    #[allow(clippy::many_single_char_names)]
+    #[allow(clippy::needless_range_loop)]
     pub fn noise(&self, p: Point3) -> f64 {
-        let i = (((4. * p.x) as isize) & 255) as usize;
-        let j = (((4. * p.y) as isize) & 255) as usize;
-        let k = (((4. * p.z) as isize) & 255) as usize;
-        self.randfloat[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        let u = p.x - p.x.floor();
+        let v = p.y - p.y.floor();
+        let w = p.z - p.z.floor();
+
+        let i = p.x.floor() as i32;
+        let j = p.y.floor() as i32;
+        let k = p.z.floor() as i32;
+
+        let mut c: [[[f64; 2]; 2]; 2] = Default::default();
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.randfloat[self.perm_x[((i + di as i32) & 255) as usize]
+                        ^ self.perm_y[((j + dj as i32) & 255) as usize]
+                        ^ self.perm_z[((k + dk as i32) & 255) as usize]]
+                }
+            }
+        }
+
+        Perlin::trilinear_interp(c, u, v, w)
     }
+
+    #[allow(clippy::needless_range_loop)]
+    fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1 - i) as f64 * (1. - u))
+                        * (j as f64 * v + (1 - j) as f64 * (1. - v))
+                        * (k as f64 * w + (1 - k) as f64 * (1. - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accum
+    }
+    //        static double trilinear_interp(double c[2][2][2], double u, double v, double w) {
+    //            auto accum = 0.0;
+    //            for (int i=0; i < 2; i++)
+    //                for (int j=0; j < 2; j++)
+    //                    for (int k=0; k < 2; k++)
+    //                        accum += (i*u + (1-i)*(1-u))*
+    //                                (j*v + (1-j)*(1-v))*
+    //                                (k*w + (1-k)*(1-w))*c[i][j][k];
+    //
+    //            return accum;
+    //        }
 }
